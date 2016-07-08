eac3db244a2af3f5833cf603a954013cacf4493d
bharatme16@hotmail.com <bharatme16@hotmail.com>
2016-07-07 20:03:07 -0500

    new library files added

--- businessLibrary/businessLibrary/Map/mapManager.vb
+++ businessLibrary/businessLibrary/Map/mapManager.vb
@@ -11,75 +11,74 @@ Imports System.Net
 'Imports iTextSharp.text.pdf
 Imports System.Collections.Generic
 Imports System.Linq
-'Imports System.Web.UI
-'Imports System.Web.UI.WebControls
-'Imports iTextSharp.text
-'Imports Artem.Google.UI
+Imports iTextSharp.text
+Imports Artem.Google.UI
+
 'Imports net.symbility.www
 'Imports CrossCountryDataAccess
 
 Public Class mapManager
-    'Get latitude and longitude by Address
-    'Public Function AddressToLatLongGeocoder(ByVal address As String) As LatLng
-    '    Dim wc As New WebClient
-    '    Dim geocodeStream As Stream =
-    '        wc.OpenRead("http://maps.googleapis.com/maps/api/geocode/xml?address=" & address & "&sensor=true")
-    '    Dim latArray As New ArrayList
-    '    Dim longArray As New ArrayList
-    '    Dim xr As New XmlTextReader(geocodeStream)
+    '   Get latitude And longitude by Address
+    Public Function AddressToLatLongGeocoder(ByVal address As String) As LatLng
+        Dim wc As New WebClient
+        Dim geocodeStream As Stream =
+            wc.OpenRead("http://maps.googleapis.com/maps/api/geocode/xml?address=" & address & "&sensor=true")
+        Dim latArray As New ArrayList
+        Dim longArray As New ArrayList
+        Dim xr As New XmlTextReader(geocodeStream)
 
-    '    Try
-    '        Dim ll As New LatLng
-    '        While xr.Read()
-    '            If xr.Name = "lat" Then
-    '                latArray.Add(xr.ReadInnerXml)
-    '                ' ll.Latitude = CDbl(xr.ReadInnerXml)
-    '            End If
-    '            If xr.Name = "lng" Then
-    '                longArray.Add(xr.ReadInnerXml)
-    '                '  ll.Longitude = CDbl(xr.ReadInnerXml)
-    '            End If
-    '        End While
-    '        ll.Latitude = CDbl(latArray(0))
-    '        ll.Longitude = CDbl(longArray(0))
-    '        wc.Dispose()
-    '        Return ll
-    '    Catch
-    '        Return Nothing
-    '    End Try
+        Try
+            Dim ll As New LatLng
+            While xr.Read()
+                If xr.Name = "lat" Then
+                    latArray.Add(xr.ReadInnerXml)
+                    ' ll.Latitude = CDbl(xr.ReadInnerXml)
+                End If
+                If xr.Name = "lng" Then
+                    longArray.Add(xr.ReadInnerXml)
+                    '  ll.Longitude = CDbl(xr.ReadInnerXml)
+                End If
+            End While
+            ll.Latitude = CDbl(latArray(0))
+            ll.Longitude = CDbl(longArray(0))
+            wc.Dispose()
+            Return ll
+        Catch
+            Return Nothing
+        End Try
 
-    'End Function
+    End Function
 
-    ''Get full address by latitude and longitude
-    'Public Function latLongToZipCodeGeocoder(ByVal latitude As String, ByVal longitude As String) As String
-    '    Dim geoCoderRequest As HttpWebRequest = Nothing
-    '    Dim geoCoderResponse As HttpWebResponse = Nothing
-    '    Dim geoCoderReader As StreamReader = Nothing
-    '    Dim readAddress As String = Nothing
-    '    Dim fullAddress As String = ""
-    '    Try
-    '        geoCoderRequest = DirectCast(WebRequest.Create("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=false"), HttpWebRequest)
-    '        geoCoderResponse = DirectCast(geoCoderRequest.GetResponse(), HttpWebResponse)
-    '        geoCoderReader = New StreamReader(geoCoderResponse.GetResponseStream())
-    '        readAddress = geoCoderReader.ReadToEnd()
-    '        geoCoderResponse.Close()
-    '        If readAddress.Contains("ZERO_RESULTS") Then
-    '            'Do nothing
-    '        End If
-    '        If readAddress.Contains("formatted_address") Then
-    '            Dim start As Integer = readAddress.IndexOf("formatted_address") + 1
-    '            Dim endIndex As Integer = readAddress.IndexOf(", USA")
-    '            Dim AddStart As String = readAddress.Substring(start + 21)
-    '            Dim EndStart As String = readAddress.Substring(endIndex)
-    '            fullAddress = AddStart.Replace(EndStart, "")
-    '        End If
-    '    Catch ex As Exception
-    '        Dim Message As String = "Error: " + ex.ToString()
-    '    Finally
-    '        If (geoCoderResponse IsNot Nothing) Then
-    '            geoCoderResponse.Close()
-    '        End If
-    '    End Try
-    '    Return fullAddress
-    'End Function
+    'Get full address by latitude and longitude
+    Public Function latLongToZipCodeGeocoder(ByVal latitude As String, ByVal longitude As String) As String
+        Dim geoCoderRequest As HttpWebRequest = Nothing
+        Dim geoCoderResponse As HttpWebResponse = Nothing
+        Dim geoCoderReader As StreamReader = Nothing
+        Dim readAddress As String = Nothing
+        Dim fullAddress As String = ""
+        Try
+            geoCoderRequest = DirectCast(WebRequest.Create("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=false"), HttpWebRequest)
+            geoCoderResponse = DirectCast(geoCoderRequest.GetResponse(), HttpWebResponse)
+            geoCoderReader = New StreamReader(geoCoderResponse.GetResponseStream())
+            readAddress = geoCoderReader.ReadToEnd()
+            geoCoderResponse.Close()
+            If readAddress.Contains("ZERO_RESULTS") Then
+                'Do nothing
+            End If
+            If readAddress.Contains("formatted_address") Then
+                Dim start As Integer = readAddress.IndexOf("formatted_address") + 1
+                Dim endIndex As Integer = readAddress.IndexOf(", USA")
+                Dim AddStart As String = readAddress.Substring(start + 21)
+                Dim EndStart As String = readAddress.Substring(endIndex)
+                fullAddress = AddStart.Replace(EndStart, "")
+            End If
+        Catch ex As Exception
+            Dim Message As String = "Error: " + ex.ToString()
+        Finally
+            If (geoCoderResponse IsNot Nothing) Then
+                geoCoderResponse.Close()
+            End If
+        End Try
+        Return fullAddress
+    End Function
 End Class
---
+++ businessLibrary/businessLibrary/PDF/pdfManager.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports iTextSharp
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+
+Public Class pdfManager
+    'Merge Pdf files
+    Public Sub MergeFiles(ByVal destinationFile As String, ByVal arrayFiles As ArrayList)
+        'Note: The merging is based on the list of the files path
+
+        'To change the order of files to be merged
+        'The order in which the files is inserted into the list need to be changed in the <Array List>.
+
+        ' Create a Document object 
+        'Size A4
+        'margin: 50,50,25,25
+        Dim document = New Document(PageSize.LETTER, 50, 50, 25, 25)
+
+        ' Create a new PdfWrite object, writing the output to a MemoryStream
+        Dim output = New MemoryStream()
+
+        'Create the file
+        Dim writer = PdfWriter.GetInstance(document, New FileStream(destinationFile, FileMode.Create))
+
+        'Open the document
+        document.Open()
+
+
+        Try
+            'First index of the file is 0
+            Dim f As Integer = 0
+
+            'Create the reader: read the first document 
+            Dim reader As New PdfReader(arrayFiles.Item(f).ToString)
+
+            'Retrieve total # of pages in the document
+            Dim n As Integer = reader.NumberOfPages
+
+            'Read the content of the document
+            Dim cb As PdfContentByte = writer.DirectContent
+
+            'Create new page
+            Dim page As PdfImportedPage
+
+            'Rotation: LandScape/portrait
+            Dim rotation As Integer
+
+            'Add the content
+            'While the document count is less than the list provided
+            While f < arrayFiles.Count
+
+                Dim i As Integer = 0
+                'Page number 
+                While i < n
+                    i += 1
+                    document.SetPageSize(reader.GetPageSizeWithRotation(i))
+                    'create new page
+                    document.NewPage()
+                    'Insert page 
+                    page = writer.GetImportedPage(reader, i)
+                    'rotate the page
+                    rotation = reader.GetPageRotation(i)
+                    If rotation = 90 OrElse rotation = 270 Then
+                        cb.AddTemplate(page, 0, -1.0F, 1.0F, 0, 0,
+                         reader.GetPageSizeWithRotation(i).Height)
+                    Else
+                        cb.AddTemplate(page, 1.0F, 0, 0, 1.0F, 0,
+                         0)
+                    End If
+                End While
+                f += 1
+                'Check the document count
+                If f < arrayFiles.Count Then
+                    'if next document exist read
+                    reader = New PdfReader(arrayFiles.Item(f).ToString)
+                    'Retreive number of pages
+                    n = reader.NumberOfPages
+                End If
+
+            End While
+            'Close the document
+            document.Close()
+        Catch ex As Exception
+            Dim strOb As String = ex.Message
+        End Try
+
+    End Sub
+
+    'Page Number
+    Public Function CountPageNo(ByVal strFileName As String) As Integer
+        'Read the file from the given path
+        Dim reader As New PdfReader(strFileName)
+
+        ' Retrieve total number of pages
+        Return reader.NumberOfPages
+    End Function
+End Class
