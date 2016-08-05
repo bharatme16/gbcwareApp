597ebc07da1f3fecbbbb7244a0f8d75c912e4238
chriscur05 <chriscur05@gmail.com>
2016-08-04 20:44:22 -0500

    added search page

---
+++ crossCountry-responsive/crossCountry-responsive/Admin/search.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="search.aspx.vb" Inherits="crossCountry_responsive.search" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<div class="panel panel-default">
+
+<div class="form-group">
+
+<div class="col-md-6 col-sm-6 col-xs-12">
+
+Insured Name:               
+<asp:TextBox ID="txtInsuredName" runat="server" Width="200px"></asp:TextBox>                           
+<br />                  
+Claim Number:
+<asp:TextBox ID="txtClaimNumber" runat="server" Width="200px"></asp:TextBox>
+<br /> 
+Policy Number:
+<asp:TextBox ID="txtPolicyNumber" runat="server" Width="200px"></asp:TextBox>
+ <br />  
+<asp:Button ID="btnSelect" runat="server" CausesValidation="False"  Text="Filter"  CssClass="btn btn-default"/>              
+</div>
+</div>
+        
+
+<div class="panel-body">
+<asp:GridView ID="gdClaims" runat="server" Width="100%" CssClass="table table-hover table-striped" GridLines="None"  
+AllowPaging="true" pagesize="50" DataKeyNames="Claim ID" ShowHeaderWhenEmpty="true"  
+AutoGenerateColumns="false" AllowSorting="true" OnSorting="sortRecords" EmptyDataText="There are no claims to display"> 
+<Columns>
+<asp:BoundField DataField="Claim ID" HeaderText="Claim ID" ReadOnly="True" SortExpression="Claim ID" HeaderStyle-CssClass="hidden" ItemStyle-CssClass="hidden"/> 
+<asp:hyperLinkField DataTextField="Claim #" HeaderText="Claim #" SortExpression="Claim #" DataNavigateUrlFields="Claim ID" DataNavigateUrlFormatString="/Admin/claimDetail.aspx?claimId={0}" HeaderStyle-CssClass="visible-lg visible-md visible-sm visible-xs" ItemStyle-CssClass="visible-lg visible-md visible-sm visible-xs"  /> 
+<asp:BoundField DataField="Insured Name" HeaderText="Insured" ReadOnly="True" SortExpression="Insured Name"/> 
+<asp:BoundField DataField="Carrier" HeaderText="Carrier" ReadOnly="True" SortExpression="Carrier" HeaderStyle-CssClass="visible-lg visible-md visible-sm visible-xs" ItemStyle-CssClass="visible-lg visible-md visible-sm visible-xs"  /> 
+<asp:BoundField DataField="Claim Contact" HeaderText="Claim Contact" ReadOnly="True" SortExpression="Claim Contact" HeaderStyle-CssClass="visible-lg visible-md" ItemStyle-CssClass="visible-lg visible-md" /> 
+<asp:BoundField DataField="Loss Type" HeaderText="Loss Type" ReadOnly="True" SortExpression="Loss Type" HeaderStyle-CssClass="visible-lg" ItemStyle-CssClass="visible-lg"  /> 
+<asp:BoundField DataField="Adjuster" HeaderText="Adjuster" ReadOnly="True" SortExpression="Adjuster" HeaderStyle-CssClass="visible-md visible-lg visible-sm" ItemStyle-CssClass="visible-md visible-lg visible-sm" />  
+<asp:BoundField DataField="Received Date" HeaderText="Received Date" ReadOnly="True" SortExpression="Received Date" HeaderStyle-CssClass="visible-md visible-lg visible-sm visible-xs" ItemStyle-CssClass="visible-md visible-lg visible-sm visible-xs"  /> 
+<asp:BoundField DataField="Status" HeaderText="Status" ReadOnly="True" SortExpression="Status" HeaderStyle-CssClass="visible-lg visible-md" ItemStyle-CssClass="visible-lg visible-md"  /> 
+
+</Columns>
+
+</asp:GridView>
+
+
+</div>
+
+</div>
+
+</asp:Content>
---
+++ crossCountry-responsive/crossCountry-responsive/Admin/search.aspx.vb
@@ -0,0 +1,110 @@
+﻿Public Class search
+    Inherits System.Web.UI.Page
+    Dim objacess As New CrossCountryDataAccess.dataAccess
+    Dim searchString As String
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+
+        End If
+    End Sub
+
+    'select button pressed
+    Protected Sub btnSelect_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSelect.Click
+
+        Dim claimNumber As String
+        Dim policyNumber As String
+        Dim insuredName As String
+
+        If txtClaimNumber.Text = String.Empty Then
+            claimNumber = "NA"
+        Else
+            claimNumber = txtClaimNumber.Text
+        End If
+        If txtInsuredName.Text = String.Empty Then
+            insuredName = "NA"
+        Else
+            insuredName = txtInsuredName.Text
+        End If
+        If txtPolicyNumber.Text = String.Empty Then
+            policyNumber = "NA"
+        Else
+            policyNumber = txtPolicyNumber.Text
+        End If
+
+        bindGridView(insuredName, claimNumber, policyNumber)
+
+    End Sub
+
+    'bind gridview
+    Protected Sub bindGridView(ByVal insuredName As String, ByVal claimNumber As String, ByVal policyNumber As String)
+        Dim dtReports As New DataTable
+        ViewState("claimNumber") = claimNumber
+        ViewState("policyNumber") = policyNumber
+        ViewState("insuredName") = insuredName
+
+        Try
+            dtReports = objacess.searchAllClaimsByCriteria(policyNumber, claimNumber, insuredName)
+            If dtReports.Rows.Count >= 1 Then
+                gdClaims.DataSource = dtReports
+                gdClaims.DataBind()
+            Else
+                gdClaims.DataSource = dtReports
+                gdClaims.DataBind()
+            End If
+
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    'Sort Records
+    Protected Sub sortRecords(sender As Object, e As GridViewSortEventArgs)
+        Dim sortExpression As String = e.SortExpression
+        Dim direction As String = String.Empty
+
+        If SortDirection = SortDirection.Ascending Then
+            SortDirection = SortDirection.Descending
+            direction = " DESC"
+        Else
+            SortDirection = SortDirection.Ascending
+            direction = " ASC"
+        End If
+
+
+
+        Dim dt As New DataTable
+
+        dt = objacess.searchAllClaimsByCriteria(ViewState("policyNumber").ToString, ViewState("claimNumber").ToString, ViewState("insuredName").ToString)
+        If dt.Rows.Count >= 1 Then
+            ViewState("SortExpr") = sortExpression & direction
+            dt.DefaultView.Sort = sortExpression & direction
+            gdClaims.DataSource = dt
+            gdClaims.DataBind()
+        End If
+
+    End Sub
+
+    'Sort Direction
+    Public Property SortDirection() As SortDirection
+        Get
+            If ViewState("SortDirection") Is Nothing Then
+                ViewState("SortDirection") = SortDirection.Ascending
+            End If
+            Return DirectCast(ViewState("SortDirection"), SortDirection)
+        End Get
+        Set
+            ViewState("SortDirection") = Value
+        End Set
+    End Property
+
+    'paging
+    Private Sub gdFiles_PageIndexChanging(sender As Object, e As GridViewPageEventArgs) Handles gdClaims.PageIndexChanging
+        Dim dt As New DataTable
+        dt = objacess.searchAllClaimsByCriteria(ViewState("policyNumber").ToString, ViewState("claimNumber").ToString, ViewState("insuredName").ToString)
+        gdClaims.PageIndex = e.NewPageIndex
+        dt.DefaultView.Sort = ViewState("SortExpr").ToString
+        gdClaims.DataSource = dt
+        gdClaims.DataBind()
+    End Sub
+End Class
